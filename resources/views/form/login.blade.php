fd78c0a066a5367bd5676501e98372d3ddee8f05
bhoraniya007 <bhoraniyamahediali3@gmail.com>
2025-09-26 13:01:42 +0530

    first commit

--- resources/views/form/login.blade.php
+++
@@ -1,8 +0,0 @@
-<div>
-    <form action="" method="post">
-        <input type="text" name="username"/>
-        <input type="text" name="city"/>
-        <input type="text" name="email"/>
-
-    <!-- People find pleasure in different ways. I find it in keeping my mind clear. - Marcus Aurelius -->
-</div>
